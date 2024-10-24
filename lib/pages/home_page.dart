0a43c789106cda0b317324af6231eed55d45d8e9
Azizbek <azizbek.yoqubov.99@gmail.com>
2024-10-24 15:06:50 +0300

    for video 3 | Image widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,6 +17,8 @@ class HomePage extends StatelessWidget {
             Image.network("https://digitalhub.fifa.com/transform/c6e4f60d-d262-413a-b54a-27fa565eb934/England-v-France-Quarter-Final-FIFA-World-Cup-Qatar-2022?io=transform:fill,width:50,width:720&quality=70"),
             Image.asset("assets/images/nature1.jpg"),
 
+
+
           ],
         ),
       ),
